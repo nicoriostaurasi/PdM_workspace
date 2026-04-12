c8021a615afd6955c629d7f302644a4215218b78
Nicolas Rios Taurasi <nicolas.rios.taurasi@gmail.com>
2026-04-11 23:11:04 -0300

    Add initial project files for STM32F446RETx including IOC and linker scripts
    - Created Nivel_Digital.ioc for project configuration with STM32F446RETx settings.
    - Added STM32F446RETX_FLASH.ld linker script for FLASH memory configuration.
    - Added STM32F446RETX_RAM.ld linker script for RAM memory configuration.

---
+++ Nivel_Digital/Drivers/API/Inc/API_cmdparser.h
@@ -0,0 +1,40 @@
+/*
+ * API_cmdparser.h
+ *
+ *  Created on: 3 abr 2026
+ *      Author: nicol
+ */
+
+#ifndef API_INC_API_CMDPARSER_H_
+#define API_INC_API_CMDPARSER_H_
+
+/* Definiciones de constantes para el parser de comandos */
+#define CMD_MAX_LINE 64
+#define CMD_MAX_TOKENS 3
+
+/** @brief Tipos de estado para la máquina de estados del parser de comandos */
+typedef enum {
+	CMD_OK = 0,
+	CMD_ERR_OVERFLOW,
+	CMD_ERR_SYNTAX,
+	CMD_ERR_UNKNOWN,
+	CMD_ERR_ARG,
+} cmd_status_t;
+
+/**
+ * @brief Inicializa el módulo parser de comandos
+ */
+void cmdParserInit(void);
+
+/**
+ * @brief Máquina de estados del Paser. Debe ser llamada periódicamente desde el bucle
+ * 		  Procesa hasta 16 bytes por invocación (no bloqueante).
+ */
+void cmdPoll(void);
+
+/**
+ * @brief Imprime por UART la lista de comandos disponibles
+ */
+void cmdPrintHelp(void);
+
+#endif /* API_INC_API_CMDPARSER_H_ */
---
+++ Nivel_Digital/Drivers/API/Src/API_cmdparser.c
@@ -0,0 +1,313 @@
+/*
+ * API_cmdparser.c
+ *
+ *  Created on: 3 abr 2026
+ *      Author: nicol
+ */
+
+#include "API_cmdparser.h"
+#include "API_uart.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "stm32f4xx_hal.h"
+
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
+
+/** @brief Tipo de función para manejar comandos */
+typedef void (*command_handler_t)(uint8_t argc, char *argv[]);
+
+/** @brief Tipos de estado para la máquina de estados del parser de comandos */
+typedef enum {
+	CMD_IDLE = 0,
+	CMD_RECEIVING,
+	CMD_PROCESS,
+	CMD_EXEC,
+	CMD_ERROR,
+} cmdParserState_t;
+
+/** @brief Estructura para definir un comando */
+typedef struct{
+    const char *name;
+	uint8_t expectedArgs;
+	command_handler_t handler;
+} command2_t;
+
+/** @brief Buffer para recibir datos por UART */
+static uint8_t uartRxBuffer[CMD_MAX_LINE];
+
+/** @brief Máquina de estados del parser de comandos */
+static cmdParserState_t cmdParserStateFsm = CMD_IDLE;
+
+/** @brief Variables para almacenar los argumentos actuales y el handler del comando a ejecutar */
+static char* currentArgv[CMD_MAX_TOKENS];
+
+/** @brief Variable para almacenar el número de argumentos actuales */
+static uint8_t currentArgc = 0;
+
+/** @brief Variable para almacenar el handler del comando a ejecutar */
+static command_handler_t currentCmdHandler;
+
+/** @brief Acción para el comando HELP */
+static void helpAction(uint8_t argc, char *argv[])
+{
+	cmdPrintHelp();
+}
+
+/** @brief Acción para el comando LED */
+static void ledAction(uint8_t argc, char *argv[])
+{
+    if (strcmp(argv[1], "ON") == 0) {
+    	HAL_GPIO_WritePin(LD2_GPIO_Port,LD2_Pin,GPIO_PIN_SET);
+		uartSendString((uint8_t*)"\nChanged LED status to ON!\r\n");
+    } else if (strcmp(argv[1], "OFF") == 0) {
+    	HAL_GPIO_WritePin(LD2_GPIO_Port,LD2_Pin,GPIO_PIN_RESET);
+    	uartSendString((uint8_t*)"\nChanged LED status to OFF!\r\n");
+    } else if (strcmp(argv[1], "TOGGLE") == 0) {
+    	HAL_GPIO_TogglePin(LD2_GPIO_Port,LD2_Pin);
+		uartSendString((uint8_t*)"\nToggled current LED STATUS!\r\n");
+	} else {
+		uartSendString((uint8_t*)"\nLED Command Bad Arguments...\r\n");
+    }
+}
+
+/**** Accion para el comando STATUS ****/
+static void statusAction(uint8_t argc, char *argv[]) {
+	GPIO_PinState ret=HAL_GPIO_ReadPin(LD2_GPIO_Port,LD2_Pin);
+	if(ret == GPIO_PIN_SET) {
+		uartSendString((uint8_t*)"\nCurrent LED STATUS: ON\r\n");
+	} else {
+		uartSendString((uint8_t*)"\nCurrent LED STATUS: OFF\r\n");
+	}
+}
+
+/** @brief Acción para el comando BAUD? */
+static void baudGetAction(uint8_t argc, char *argv[]) {
+	uint32_t currentBaudrate = getCurrentBaudrate();
+	char buffBaud[8];
+
+	sprintf(buffBaud, "%lu", currentBaudrate);
+	uartSendString((uint8_t*)"\nCurrent Baudrate: ");
+	uartSendString((uint8_t*)buffBaud);
+	uartSendString((uint8_t*)"\r\n");
+}
+
+/** @brief Acción para el comando BAUD= */
+static void baudSetAction(uint8_t argc, char *argv[]) {
+	uint32_t newBaudrate = atoi(argv[1]);
+
+	if(newBaudrate<=921600 && newBaudrate>=9600 ){
+		uartSendString((uint8_t*)"\nChange current Baudrate to: ");
+		uartSendString((uint8_t*)argv[1]);
+		uartSendString((uint8_t*)"\r\n");
+		changeCurrentBaudrate(newBaudrate);
+	} else {
+		uartSendString((uint8_t*)"\nERROR: invalid baudrate to change\r\n");
+	}
+
+}
+
+/** @brief Función para tokenizar la línea de comando recibida
+ *  @param input: La línea de comando a tokenizar
+ *  @param argv: El array donde se almacenarán los tokens
+ *  @return: El número de tokens encontrados, o -1 si hubo un error de overflow
+ */
+static int8_t tokenize(char *input, char *argv[])
+{
+    uint8_t argc = 0;
+    char *token = strtok(input, " ");
+
+    while (token != NULL) {
+        if (argc >= CMD_MAX_TOKENS) {
+            return -1;
+        }
+
+		argv[argc++] = token;
+        token = strtok(NULL, " ");
+    }
+    return argc;
+}
+
+/** @brief Función para ejecutar la acción correspondiente a un error de comando
+ *  @param errorAction: El tipo de error ocurrido
+ */
+static void cmdExecutError(cmd_status_t errorAction){
+	switch(errorAction){
+		case CMD_ERR_OVERFLOW: {
+			uartSendString((uint8_t*)"\n\rERROR: line too long\r\n");
+			break;
+		}
+		case CMD_ERR_SYNTAX: {
+			uartSendString((uint8_t*)"\n\rERROR: Syntax error\r\n");
+			break;
+		}
+		case CMD_ERR_UNKNOWN: {
+			uartSendString((uint8_t*)"\n\rERROR: Unknown command\r\n");
+			break;
+		}
+		case CMD_ERR_ARG: {
+			uartSendString((uint8_t*)"\n\rERROR: Bad arguments\r\n");
+			break;
+		}
+		default: {
+			uartSendString((uint8_t*)"\n\rERROR: Unknown command\r\n");
+			break;
+		}
+	}
+}
+
+/** @brief Tabla de comandos disponibles */
+const command2_t commandTable2[] = {
+	{"HELP",1, helpAction},
+	{"LED",2, ledAction},
+	{"STATUS",1, statusAction},
+	{"BAUD?",1, baudGetAction},
+	{"BAUD=",2, baudSetAction},
+};
+
+/** @brief Función para procesar la línea de comando recibida y ejecutar la acción correspondiente
+ *  @return: El estado del procesamiento del comando
+ */
+static cmd_status_t cmdProcessLine(void)
+{
+	char* argv[CMD_MAX_TOKENS];
+	int8_t argc = tokenize((char*)uartRxBuffer, argv);
+
+	// Valido que los comandos sean los aceptados, si hay mas doy un overflow
+    if (argc < 0) {
+        return CMD_ERR_OVERFLOW;
+    }
+
+    // Valido que no sea una linea vacía
+    if (argc == 0) {
+        return CMD_ERR_SYNTAX;
+    }
+
+    // Recorro para checkear dinamicamente la lista de comandos disponibles
+    for (uint8_t i = 0; i < (sizeof(commandTable2) / sizeof(commandTable2[0])); i++) {
+		if (strcmp(argv[0], commandTable2[i].name) == 0) {
+			if(commandTable2[i].expectedArgs == argc){
+				currentArgc = argc;
+				memcpy(&currentArgv, &argv, sizeof(char*) * argc);
+				currentCmdHandler = commandTable2[i].handler;
+				return CMD_OK;
+			} else{
+				return CMD_ERR_ARG;
+			}
+		}
+    }
+
+    // no existe comando
+    return CMD_ERR_UNKNOWN;
+}
+
+/**
+ * @brief Ejecuta la accion correspondiente
+ */
+static void cmdExecutAction(void){
+	currentCmdHandler(currentArgc, currentArgv);
+}
+
+/**
+ * @brief Inicializa el módulo parser de comandos
+ */
+void cmdParserInit(void){
+	cmdParserStateFsm = CMD_IDLE;
+	currentArgc = 0;
+	memset(uartRxBuffer,0,CMD_MAX_LINE);
+}
+
+/**
+ * @brief Máquina de estados del Paser. Debe ser llamada periódicamente desde el bucle
+ * 		  Procesa hasta 16 bytes por invocación (no bloqueante).
+ */
+void cmdPoll(void){
+	static uint8_t currentDataIndex = 0;
+	static cmd_status_t cmdProcessStatus;
+	uint8_t aux;
+	switch(cmdParserStateFsm) {
+		case CMD_IDLE:{
+			uartReceiveStringSize(&aux, 1);
+			if(isNewDataOnRx()){
+				uartSendStringSize(&aux,1);
+				// Si detecto un caracter válido de un nuevo comando, lo guardo y cambio de estado
+				if (aux != '\r' && aux != '\n') {
+					cmdParserStateFsm = CMD_RECEIVING;
+					// limpio el buffer por si hay basura
+					memset(uartRxBuffer,0,CMD_MAX_LINE);
+					// Almaceno el primer caracter
+					uartRxBuffer[0] = (char)toupper((unsigned char)aux);
+					currentDataIndex = 1;
+				}
+			}
+			break;
+		}
+
+		case CMD_RECEIVING:{
+			uartReceiveStringSize(&aux, 1);
+			if(isNewDataOnRx()){
+				uartSendStringSize(&aux,1);
+				if (aux == '\r' || aux == '\n'){
+					// Una vez que se recibe el último caracter se procesa la trama
+					cmdParserStateFsm = CMD_PROCESS;
+					uartRxBuffer[currentDataIndex] = '\0';
+					currentDataIndex++;
+				}else{
+					// Si hay caracteres válidos se continua almacenando
+					uartRxBuffer[currentDataIndex] = (char)toupper((unsigned char)aux);
+					currentDataIndex++;
+				}
+			}
+
+			if(currentDataIndex>=CMD_MAX_LINE){
+				cmdProcessStatus = CMD_ERR_OVERFLOW;
+				cmdParserStateFsm = CMD_ERROR;
+			}
+			break;
+		}
+
+		case CMD_PROCESS:{
+			cmdProcessStatus = cmdProcessLine();
+			if(cmdProcessStatus == CMD_OK){
+				cmdParserStateFsm = CMD_EXEC;
+			} else {
+				cmdParserStateFsm = CMD_ERROR;
+			}
+			break;
+		}
+
+		case CMD_EXEC:{
+			cmdExecutAction();
+			cmdParserStateFsm = CMD_IDLE;
+			break;
+		}
+
+		case CMD_ERROR:{
+			cmdExecutError(cmdProcessStatus);
+			cmdParserStateFsm = CMD_IDLE;
+			break;
+		}
+
+		default:{
+			cmdParserStateFsm = CMD_IDLE;
+			break;
+		}
+
+	}
+	return;
+}
+
+/**
+ * @brief Imprime por UART la lista de comandos disponibles
+ */
+void cmdPrintHelp(void){
+	uartSendString((uint8_t*)"\nAvailable Commands:\r\n");
+	for (uint8_t i = 0; i < (sizeof(commandTable2) / sizeof(commandTable2[0])); i++){
+		uartSendString((uint8_t*)commandTable2[i].name);
+		uartSendString((uint8_t*)"\r\n");
+	}
+}
+
+
